f71879a90a41a5fcfae87ad9e4fadd5c306f4ce9
yd786 <lucifer.ys786@gmail.com>
2020-05-29 19:09:28 +0530

    thinkfixed232

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -3,49 +3,43 @@ import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(["data"]).toJS()
-  const entryServiceImages = entry.getIn(['data', 'services', 'images'])
-  const ServiceImages = entryServiceImages ? entryServiceImages.toJS() : []
-
   const entryServices = entry.getIn(['data', 'services', 'service'])
   const Services = entryServices ? entryServices.toJS() : []
 
   const entryWorkLogos = entry.getIn(['data', 'work', 'logos'])
   const WorkLogos = entryWorkLogos ? entryWorkLogos.toJS() : []
 
-  if (data) {
     return (
       <IndexPageTemplate
-        bgimage={getAsset(data.getIn(['data', 'bgimage']))}
-        siteName={data.getIn(['data', 'siteName'])}
-        heading={data.getIn(['data', 'heading'])}
-        subheading={data.getIn(['data', 'subheading'])}
+        bgimage={getAsset(entry.getIn(['data', 'bgimage']))}
+        siteName={entry.getIn(['data', 'siteName'])}
+        heading={entry.getIn(['data', 'heading'])}
+        subheading={entry.getIn(['data', 'subheading'])}
         calltoaction={{
-          title:data.getIn(['data', 'calltoaction','title']),
-          description:data.getIn(['data', 'calltoaction','description']),
+          title:entry.getIn(['data', 'calltoaction','title']),
+          description:entry.getIn(['data', 'calltoaction','description']),
         }}
         services={{
-          images:ServiceImages,
+          image1:getAsset(entry.getIn(['data', 'bgimage'])),
+          image2:getAsset(entry.getIn(['data', 'bgimage'])),
+          image3:getAsset(entry.getIn(['data', 'bgimage'])),
           service:Services
         }}
         work={{
-          headerImage: getAsset(data.getIn(['data', 'work', 'headerImage'])),
+          headerImage: getAsset(entry.getIn(['data', 'work', 'headerImage'])),
           logos : WorkLogos
         }}
         about={{
-          sideImage:getAsset(data.getIn(['data', 'about', 'sideImage'])),
-          description:getIn(['data','about','description'])
+          sideImage:getAsset(entry.getIn(['data', 'about', 'sideImage'])),
+          description:entry.getIn(['data','about','description'])
         }}
         footer={{
-          address:data.getIn(['data', 'footer','address']),
-          contact:data.getIn(['data', 'footer','contact']),
-          phone:data.getIn(['data', 'footer','phone']),
+          address:entry.getIn(['data', 'footer','address']),
+          contact:entry.getIn(['data', 'footer','contact']),
+          phone:entry.getIn(['data', 'footer','phone']),
         }}
       />
     )
-  } else {
-    return <div>Loading...</div>
-  }
 }
 
 IndexPagePreview.propTypes = {

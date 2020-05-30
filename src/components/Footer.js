2800381252f3b5deb48ad074fda235cfd50e0b4a
yd786 <lucifer.ys786@gmail.com>
2020-05-30 10:02:22 +0530

    exe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import facebook from "../img/social/facebook.svg"
-import instagram from "../img/social/instagram.svg"
-import twitter from "../img/social/twitter.svg"
-import vimeo from "../img/social/vimeo.svg"
+
 
 const Footer = class extends React.Component {
   render() {
@@ -68,7 +65,6 @@ const Footer = class extends React.Component {
               <div className="column is-4 social">
                 <a title="facebook" href="https://facebook.com">
                   <img
-                    src={facebook}
                     alt="Facebook"
                     style={{ width: "1em", height: "1em" }}
                   />
@@ -76,21 +72,18 @@ const Footer = class extends React.Component {
                 <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
-                    src={twitter}
                     alt="Twitter"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
                 <a title="instagram" href="https://instagram.com">
                   <img
-                    src={instagram}
                     alt="Instagram"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
                 <a title="vimeo" href="https://vimeo.com">
                   <img
-                    src={vimeo}
                     alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
